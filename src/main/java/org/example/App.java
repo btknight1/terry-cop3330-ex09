ff992da069a0b57f1852ad5369b91fed72e6c1bb
Brandon Terry <brandon2024@knights.ucf.edu>
2021-08-31 17:47:09 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        double width;
+        double length;
+        double area;
+        double gallon = 350;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Length? ");
+        length = input.nextDouble();
+
+        System.out.print("Width? ");
+        width = input.nextDouble();
+
+        area = length * width;
+        double howMuchPaint = Math.ceil(area/gallon);
+
+        System.out.printf("You will need to purchase %.0f gallons of paint to cover %.0f square feet.", howMuchPaint, area);
+    }
+}
